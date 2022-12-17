18bd9e152e7bd3e98ae4dd124e0789a613a62ba6
torupa <xpt77@outlook.com>
2022-12-17 18:14:41 +0900

    update

--- src/conjugate_gradient.py
+++ src/conjugate_gradient.py
@@ -46,3 +46,21 @@ def solve(A, b, **args):
     if _flog is not None:
         _flog.close()
     return x
+
+if __name__ == '__main__':
+    A = np.array([[1.1, -2.1, 3.0],
+                  [0.0, 0.2,  0.5],
+                  [1.5, -0.1, 2.0]])
+    A = A * A.T
+    b = np.array([1.1, 0.1,-2.4])
+    ans = solve(A, b)
+    print(f'rank(A)={np.linalg.matrix_rank(A)}')
+    w, v = np.linalg.eig(A)
+    print(f'eigen: {w}')
+    #print(f'ans={ans}')
+    r = b - np.dot(A,ans)
+    #print(f'b=Ax={b_a}')
+    print(f'norm(b - A*x)={np.linalg.norm(r)}')
+    for v in r:
+        print(v)
+        assert np.abs(v) < 1.0E-5
